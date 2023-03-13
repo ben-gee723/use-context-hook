f3509dc50bb96be5fabaf04a96bf786c75600710
ben-gee723 <ben.m.goldschmidt@gmail.com>
2023-03-13 15:01:53 +0100

    useMyContext updated

--- useMyContext.jsx
+++ useMyContext.jsx
@@ -1,14 +1,15 @@
-import React, { createContext, useState, useContext } from "react";
+import React, { createContext, useState, useEffect, useContext } from "react";
 
 const useMyContext = ({ name, init, functions }) => {
   const InitialContext = createContext();
 
   function ParentContext({ children }) {
+    // Persist
     const ls = JSON.parse(localStorage.getItem(name));
     const initalState = ls ? ls: init;
 
     const [state, setState] = useState(initalState);
-    
+
     useEffect(()=> {
       localStorage.setItem(name, JSON.stringify(state))
     }, [state])
